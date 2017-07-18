b4ca25bf87f040d636af54bce044899b7865c4c0
tehpsalmist <ronyfan102@gmail.com>
2017-07-17 21:52:57 -0400

    beginning to create Cat class
    Moving the Cat class out of the ViewModel (sound familiar?)

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
-var ViewModel = function() {
-    self = this;
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.catLevel = ko.observable(1);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('something');
     this.nicknameArray = ko.observableArray([{nickname: 'Cutie'},{nickname: 'Fluffy'},{nickname: 'Stupid'},{nickname: 'Next'}]);
+}
+
+var ViewModel = function() {
+    this.currentCat = ko.observable(new Cat());
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
         this.levelUp();
     };
 
     this.levelUp = function() {
-        if (this.clickCount() % 20 === 0) {
-            this.catLevel(this.catLevel() + 1);
+        if (this.currentCat().clickCount() % 20 === 0) {
+            this.currentCat().catLevel(this.currentCat().catLevel() + 1);
         };
     };
 }
